21f2af7c1e1deb5ef5befc04359e69832b4c716c
JorgeBasaure <jorge.basaure.p@gmail.com>
2023-02-17 22:39:46 -0300

    0.6.1 Reestructuracion en HTML con sus DIV

--- src/PresentacionIng.js
+++ src/PresentacionIng.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import prof from './sample/Profile.json'
-
-
-
+import PicDev from './img/20230131_124854.jpg'
+import AcdDev from './img/bellavista-7___Selected.jpg'
+import JobDev from './img/1332946447-lmb-bic-mut-10.jpg'
 class PortafolioDev extends Component {
   state = { show: false }
   render() {
     if (!this.state.show) {
       return (
-        <p>{this.props.mytext}</p>
+        <p id="perfil">{this.props.mytext}</p>
       )
     }
   }
@@ -16,31 +16,39 @@ class PortafolioDev extends Component {
 
 
 class PresentacionDev extends Component {
-
+  //TODO  Poner un for para iterar cada habilidad 
+  //Hacer para cada clase de habilidad tal cual como eatá implementado este codigo.
   render() {
 
     return (
       <div className="profile">
+        <h1>Presentación</h1>
 
-        <h1 className="profile__name">Presentación  Ajuste para los DIV</h1>
-        <div className="profile--title"><p>{prof[0].title}</p></div>
-         <div className='profile__Spersonal'>
-            <div className="profile--personal"></div>
-            <div className="profile--paragraph"><PortafolioDev mytext={prof[0].description} /></div>
+        <div>
+          <p>{prof[0].title}</p>
+          <img src={PicDev} alt="" />
+        </div>
+        <div>
+
+          <PortafolioDev mytext={prof[0].description} />
         </div>
 
-        <div  className="profile--title"><p>{prof[1].title}</p></div>
-  <div className='profile__Sacademy'>
-      <div className="profile--academy"></div>
-      <div  className="profile--paragraph"><PortafolioDev mytext={prof[1].description} /></div>
-  </div>
+        <div>
+          <p>{prof[1].title}</p>
+          <img src={AcdDev} alt="" />
+        </div>
+        <div>
 
-    <div className="profile--title"><p>{prof[2].title}</p></div>    
-    <div className='profile__Sjob'>
-      <div className="profile--job"></div>
-      <div  className="profile--paragraph"><PortafolioDev mytext={prof[2].description} /></div>
-    </div>
-        
+          <PortafolioDev mytext={prof[1].description} />
+        </div>
+        <div>
+        <p>{prof[2].title}</p>
+          <img src={JobDev} alt="" />
+          </div>
+        <div>
+          
+          <PortafolioDev mytext={prof[2].description} /></div>
+
       </div>
     );
   }
