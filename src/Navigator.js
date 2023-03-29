72f83bf78644fbe548f2b913a87f9f2ad1be08fa
JorgeBasaure <jorge.basaure.p@gmail.com>
2023-03-29 13:23:18 -0300

    0.8.9.7.2 Barra de nav resp en clase funcionando.

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,65 +1,81 @@
 import React, { Component} from 'react';
 //import NavResp from '../src/NavigatorResponsive'
-
+//TODO: HACER SNIPPET UNA VEZ FUNCIONANDO ESTE  ARCHIVO
 
 class NavegadorResp extends Component
 {
     
-        state ={show: false, menu:false  }
+        state ={ 
+                        show: false, 
+                        menu:false 
+                    }
+
         render()
             {
+                const toggleMenu = !this.state.menu
+                const Menu = this.state.menu
                //console.log(this.state.menu)
                 if (!this.state.show)
                     {
                         return(
-                                
-                                <div onClick={ ()=> this.setState(   { menu: !this.state.menu }   )       } className={`Hamburger ${this.state.menu ? 'open': ''}`}>
+                        
+                        <nav  className='Navigator' id='myNavigator'> 
+                            <div className={  `Navigator__container ${  Menu ? 'open': '' } `  } >
+                                <a href="#sector_01" className='Navigator__links'>Presentación</a>
+                                <a href="#sector_02" className='Navigator__links'>Habilidades en Front-End</a>
+                                <a href="#sector_03" className='Navigator__links'>Habilidades en Back-End</a>
+                                <a href="#sector_04" className='Navigator__links'>Habilidades en IDE</a>
+                                <a href="#sector_05" className='Navigator__links'>Habilidades en Office</a>
+                                <a href="#sector_06" className='Navigator__links'>Habilidades en BB.DD.</a>
+                                <a href="#sector_07" className='Navigator__links'>Habilidades en Conceptos</a>
+                                <a href="#sector_08" className='Navigator__links'>Habilidades en Control de versiones</a>
+                                <a href="#sector_09" className='Navigator__links'>Contacto</a>
+                            </div>
+                            <div className='Navigator__responsive'>
+                                <div onClick={   ()=> this.setState(   { menu: toggleMenu }   )   }  className={  `Hamburger ${  Menu ? 'open': '' } `  }>
                                     <span></span>
                                     <span></span>
                                     <span></span>
-                                </div>     
+                                </div>  
+                                </div>
+                        </nav>
+
+
                             )
                     }                  
             }
 }
-//Equivalente a función:
-//const [menu,setMenu] = useState (false)
-   // const toggleMenu = () => { setMenu (!menu) }
-   //<div onClick={ toggleMenu } className={`Hamburger  ${menu ? 'open': ''}`}>
+
+/*Equivalente a función:
+function BurguerButton()
+{
+    return(
+                const [menu,setMenu] = useState (false)
+                const toggleMenu = () => { setMenu (!menu) }
+                <div onClick={ toggleMenu } className={`Hamburger  ${Menu ? 'open': ''}`}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>                
+    )
+}
+export default BurguerButton
+*/
+
 
 
 
 class Navegador extends Component
 {
-
-
-    state = { 
-        
-        show: false 
-        }
+    state = { show: false }
     render()
     {
         
         if (!this.state.show)
         {
         return(
-            <nav className='Navigator' id='myNavigator'>
-                <div className='Navigator__container'>
-                    <a href="#sector_01" className='Navigator__links'>Presentación</a>
-                    <a href="#sector_02" className='Navigator__links'>Habilidades en Front-End</a>
-                    <a href="#sector_03" className='Navigator__links'>Habilidades en Back-End</a>
-                    <a href="#sector_04" className='Navigator__links'>Habilidades en IDE</a>
-                    <a href="#sector_05" className='Navigator__links'>Habilidades en Office</a>
-                    <a href="#sector_06" className='Navigator__links'>Habilidades en BB.DD.</a>
-                    <a href="#sector_07" className='Navigator__links'>Habilidades en Conceptos</a>
-                    <a href="#sector_08" className='Navigator__links'>Habilidades en Control de versiones</a>
-                    <a href="#sector_09" className='Navigator__links'>Contacto</a>
-                </div>
-                <div className='Navigator__responsive'>
-                    <NavegadorResp/>
-                    </div>
-                </nav>
-                )
+            <div><NavegadorResp/></div>
+                    )
         }
     }
 }
