1fe8d85272c2f0df2d2b93b1c295f9dd357269ac
JorgeBasaure <jorge.basaure.p@gmail.com>
2023-04-04 13:59:45 -0400

    0.9.2.1.b Portafolio funcional agregando carrusel

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,58 +1,30 @@
 import React, { Component} from 'react';
 //Colocar el snippetcomplementario en SASS llamado " asds"
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("Navigator__container__mySlides");
-  var dots = document.getElementsByClassName("Navigator__dot");
-
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-}
-for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace("", "");
-}
-//slides[4-1].style.display = "block"; 
-//dots[4-1].className += " open";
-
-
-}
+//Grande = Navigator__container open
+//Punto = Navigator__dot open
 class NavegadorResp extends Component
 {
 
         state ={ 
                         show: false, 
-                        menu:false,
-                        slideIndex: 1 
+                        menu:false
                     }
 
         render()
             {
-console.log(this.state.slideIndex);
                 const toggleMenu = !this.state.menu
                 const Menu = this.state.menu
+
                 if (!this.state.show)
                     {
                         return(
                         
                         <nav  className='Navigator' id='myNavigator'> 
+                        
                             <div className={  `Navigator__container ${  Menu ? 'open': '' } `  } >
-                                <div onClick={()=>  {this.setState({slideIndex: this.state.slideIndex -1})}}  >
-                            <p className={  `Navigator__button__prev ${  Menu ? 'open': '' } `  } >❮</p>
-		                        </div>
+                            <div>
+                                        <p className={  `Navigator__button__prev ${  Menu ? 'open': '' } `  } >❮</p>
+                                    </div>
                                 <div className='Navigator__container__mySlides'> 
                                 <a onClick={   ()=> this.setState(   { menu: toggleMenu }   )   }  href="#sector_01" className='Navigator__links'>Presentación</a>
                                 </div>
@@ -79,22 +51,24 @@ console.log(this.state.slideIndex);
                                 </div>
                                 <div className='Navigator__container__mySlides'>
                                     <a onClick={   ()=> this.setState(   { menu: toggleMenu }   )   }  href="#sector_09" className='Navigator__links'>Contacto</a>
+
                                 </div>
-                                <div onClick={()=>  {this.setState({slideIndex: this.state.slideIndex +1})}}  >
-                                <p className={  `Navigator__button__next ${  Menu ? 'open': '' } `  }  >❯</p>
+                                <div>
+                                        <p  className={  `Navigator__button__next ${  Menu ? 'open': '' } `  }  >❯</p>
+                                    </div>
+                                <div className={  `Navigator__dot__container ${  Menu ? 'open': '' } `  } >
+                                <span className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></span> 
+                                <span className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></span> 
+                                <span className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></span> 
+                                <span className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></span> 
+                                <span className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></span> 
+                                <span className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></span> 
+                                <span className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></span> 
+                                <span className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></span> 
+                                <span className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></span> 
                                 </div>
 
-                                <div className={  `Navigator__dot__container ${  Menu ? 'open': '' } `  } >
-                                <span className="Navigator__dot" onClick={currentSlide(1)} ></span> 
-                                <span className="Navigator__dot" onClick={currentSlide(2)} ></span> 
-                                <span className="Navigator__dot" onClick={currentSlide(3)} ></span> 
-                                <span className="Navigator__dot" onClick={currentSlide(4)} ></span> 
-                                <span className="Navigator__dot" onClick={currentSlide(5)} ></span> 
-                                <span className="Navigator__dot" onClick={currentSlide(6)} ></span> 
-                                <span className="Navigator__dot" onClick={currentSlide(7)} ></span> 
-                                <span className="Navigator__dot" onClick={currentSlide(8)} ></span> 
-                                <span className="Navigator__dot" onClick={currentSlide(9)} ></span> 
-                                </div>
+
                             </div>
 
                             <div className='Navigator__responsive'>
@@ -109,6 +83,7 @@ console.log(this.state.slideIndex);
                                     <span></span>
                                     <span></span>
                                 </div>  
+
                                 </div>
 
                         </nav>
