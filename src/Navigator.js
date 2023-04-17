d76c0bde03edfa41924a905780ab79874429695b
JorgeBasaure <jorge.basaure.p@gmail.com>
2023-04-17 12:09:25 -0400

    0.9.7.1 Publicación en beta.

--- src/Navigator.js
+++ src/Navigator.js
@@ -9,219 +9,197 @@ import React, { Component } from 'react';
 
 class NavegadorResp extends Component
 {
-constructor( )
-    {
-        super( )
-    //Separar por tipo de dato
-        this.state = {
-            show : false, 
-            menu : false,
+        constructor( )
+        {
 
-            index : 1 
+                super( )
+                //Separar por tipo de dato
+                this.state = {
+
+                        show : false, 
+                        menu : false,
+
+                        index : 1 
+                }
         }
-    }
 
 
-    IndexDotClick = ( ) => 
-    {
-        const containerOpen = document.querySelector( '.Navigator__container__mySlides' ) ;
-        const dotOpen = document.querySelectorAll( ".Navigator__dot.open" ) ;
-        let calc = 100 / dotOpen.length ;
-        const xIndex = calc * 4 ;
-
-        dotOpen.forEach( (  cadaDotOpen , i  ) =>
+        IndexDotClick = ( ) => 
         {
 
-            dotOpen[ i ].addEventListener(  'click', ( ) => {
+                const containerOpen = document.querySelector( '.Navigator__container__mySlides' ) ;
+                const dotOpen = document.querySelectorAll( ".Navigator__dot.open" ) ;
+                let calc = 100 / dotOpen.length ;
+                const xIndex = calc * 4 ;
 
-                const op = xIndex - ( calc * i ) ;
+                dotOpen.forEach( (  cadaDotOpen , i  ) =>
+                {
 
-                    containerOpen.style.transform = `translateX( ${ op }% )` ;
+                        dotOpen[ i ].addEventListener(  'click', ( ) => 
+                        {
 
-                    dotOpen.forEach( (  cadaDotOpen  ,  i  ) => 
-                    {
+                                const op = xIndex - ( calc * i ) ;
+                                containerOpen.style.transform = `translateX( ${ op }% )` ;
 
-                            dotOpen[ i ].classList.remove( 'on' ) ;
+                                dotOpen.forEach( (  cadaDotOpen  ,  i  ) => 
+                                {
 
-                    } ) ;  
+                                                dotOpen[ i ].classList.remove( 'on' ) ;
+                                }  ) ;  
 
-                        dotOpen[ i ].classList.add( 'on' ) ;
-
-            } ) ;
-        } ) ;
-    } ;
-
-
-    mySlidesReset = ( ) =>
-        {
-            const containerOpen = document.querySelector( '.Navigator__container__mySlides' );
-
-            containerOpen.addEventListener(  'mouseover' , ( ) => 
-            {
-                containerOpen.style.transform = `translateX( ${ 0 }% )` ;
-            } ) ;
+                                dotOpen[ i ].classList.add( 'on' ) ;
+                        }  )  ;
+                }  )  ;
         } ;
 
-
-    indexArrowClickPlus = ( ) =>
-    {
-        const count = this.state.index ;
-        this.setState(  {  index : count + 1  }   ) ;   //this.setState( prevState => ( { index: prevState.index+ 1 } ))
-        let value = count + 1 ;
-        const containerOpen = document.querySelector( '.Navigator__container__mySlides' ) ;
-        const arrowOpen = document.querySelectorAll( ".Navigator__links" ) ;
-
-        if ( value > arrowOpen.length )
+        mySlidesReset = ( ) =>
         {
-            value = value - arrowOpen.length ; 
-            this.setState(  {  index : count - (  arrowOpen.length - 1  )  }  ) ;
-        }
-        let calc = 100 / arrowOpen.length ;
-        const xIndex = calc * 4 ;
-        const op = ( xIndex + calc ) + ( calc * -value ) ;
 
-            containerOpen.style.transform = `translateX( ${ op }% )` ; 
-    };
+                const containerOpen = document.querySelector( '.Navigator__container__mySlides' );
 
+                containerOpen.addEventListener(  'mouseover' , ( ) => 
+                {
+                        containerOpen.style.transform = `translateX( ${ 0 }% )` ;
+                }  )  ;
+        } ;
 
-    indexArrowClickMinus = ( ) =>
-    {
-        const count = this.state.index ;
-        this.setState(  {  index : count - 1  }  ) ;  //this.setState( prevState => ( { index: prevState.index -1 } ))
-        let value = count - 1 ;
-        let reset = 1 ;
-        const containerOpen = document.querySelector( '.Navigator__container__mySlides' ) ;
-        const arrowOpen = document.querySelectorAll( ".Navigator__links" ) ;
-
-        if ( value < reset )
+        indexArrowClickPlus = ( ) =>
         {
-            value = value + arrowOpen.length ;
-            this.setState(  {  index : count + (  arrowOpen.length - 1  )  }  ) ;
-        }
-            let calc = 100 / arrowOpen.length ;
-            const xIndex = calc * 4 ;
-            const op = ( xIndex + calc ) + ( calc * -value );
 
-            containerOpen.style.transform = `translateX( ${ op }% )` ;
+                const count = this.state.index ;
+                this.setState(  {  index : count + 1  }   ) ;   //this.setState( prevState => ( { index: prevState.index+ 1 } ))
+                let value = count + 1 ;
+                const containerOpen = document.querySelector( '.Navigator__container__mySlides' ) ;
+                const arrowOpen = document.querySelectorAll( ".Navigator__links" ) ;
+                let calc = 100 / arrowOpen.length ;
+                const xIndex = calc * 4 ;
+                const op = ( xIndex + calc ) + ( calc * -value ) ;
+                
+                if ( value > arrowOpen.length )
+                {
 
+                        value = value - arrowOpen.length ; 
+                        this.setState(  {  index : count - (  arrowOpen.length - 1  )  }  ) ;
+                }
 
-    };
+                containerOpen.style.transform = `translateX( ${ op }% )` ; 
+        };
+
+        indexArrowClickMinus = ( ) =>
+        {
+
+                const count = this.state.index ;
+                this.setState(  {  index : count - 1  }  ) ;  //this.setState( prevState => ( { index: prevState.index -1 } ))
+                let value = count - 1 ;
+                let reset = 1 ;
+                const containerOpen = document.querySelector( '.Navigator__container__mySlides' ) ;
+                const arrowOpen = document.querySelectorAll( ".Navigator__links" ) ;
+                let calc = 100 / arrowOpen.length ;
+                const xIndex = calc * 4 ;
+                const op = ( xIndex + calc ) + ( calc * -value );
+
+                if ( value < reset )
+                {
+
+                        value = value + arrowOpen.length ;
+                        this.setState(  {  index : count + (  arrowOpen.length - 1  )  }  ) ;
+                }
+
+                containerOpen.style.transform = `translateX( ${ op }% )` ;
+        };
 
         render( )
-            {
+        {
                 
                 const toggleMenu = !this.state.menu ;
                 const Menu = this.state.menu ;
                 const stateShow = this.state.show ;
 
                 if ( !stateShow )
-                    {
+                {
                         return (
 
-                        <nav  className='Navigator' id='myNavigator'> 
-                            <div className='Navigator__responsive'>
+                                <nav  className='Navigator' id='myNavigator'> 
+                                        <div className='Navigator__responsive'>
 
-<div onClick={   ()=> this.setState(   { menu: toggleMenu }   )   }  className={  `Hamburger ${  Menu ? 'open': '' } `  }>
+                                                <div onClick={   ()=> this.setState(   { menu: toggleMenu }   )   }  className={  `Hamburger ${  Menu ? 'open': '' } `  }>
 
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
+                                                        <span></span>
+                                                        <span></span>
+                                                        <span></span>
+                                                        <span></span>
+                                                        <span></span>
+                                                        <span></span>
+                                                        <span></span>
+                                                        <span></span>
+                                                        <span></span>
 
-</div>  
+                                                </div>  
 
-</div>
-
-                            <div   className={  `Navigator__container ${  Menu ? 'open': '' } `  } >
-
-                                <div className='Navigator__container__mySlides'> 
-
-                                    <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_01" className='Navigator__links'>Presentación</a>
-
-                                    <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_02" className='Navigator__links'>Habilidades en Front-End</a>
-
-                                    <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_03" className='Navigator__links'>Habilidades en Back-End</a>
-
-                                    <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_04" className='Navigator__links'>Habilidades en IDE</a>
-
-                                    <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_05" className='Navigator__links'>Habilidades en Office</a>
-
-                                    <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_06" className='Navigator__links'>Habilidades en BB.DD.</a>
-
-                                    <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_07" className='Navigator__links'>Habilidades en Conceptos</a>
-
-                                    <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_08" className='Navigator__links'>Habilidades en Control de versiones</a>
-
-                                    <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_09" className='Navigator__links'>Contacto</a>
-
-                                </div>
-
-
-                                <div>
-
-
-
-                                </div>
-
-
-                                <p   className='prueba'>{ this.state.index }</p>
-
-
-                                <div className='Navigator__listdot'>
-
-                                <div onClick={   ( )=> this.indexArrowClickMinus( )  }     className={  `Navigator__button__prev ${  Menu ? 'open': '' } `  }>
-                                            <span></span>
-                                            <span></span>
-                                            <span></span>
                                         </div>
 
+                                        <div   className={  `Navigator__container ${  Menu ? 'open': '' } `  } >
 
-                                        <div className={  `Navigator__dot__container ${  Menu ? 'open': '' } `  } >
+                                                <div className='Navigator__container__mySlides'> 
 
+                                                        <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_01" className='Navigator__links'>Presentación</a>
 
+                                                        <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_02" className='Navigator__links'>Habilidades en Front-End</a>
 
-                                    <ul >
+                                                        <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_03" className='Navigator__links'>Habilidades en Back-End</a>
 
-                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
-                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
-                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
-                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
-                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
-                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
-                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
-                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
-                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
+                                                        <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_04" className='Navigator__links'>Habilidades en IDE</a>
 
-                                    </ul>
-                                    </div>
-                                    <div onClick={   ( )=> this.indexArrowClickPlus( )   }     className={  `Navigator__button__next ${  Menu ? 'open': '' } `  }  >
-                                            <span></span>
-                                            <span></span>
-                                            <span></span>
+                                                        <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_05" className='Navigator__links'>Habilidades en Office</a>
+
+                                                        <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_06" className='Navigator__links'>Habilidades en BB.DD.</a>
+
+                                                        <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_07" className='Navigator__links'>Habilidades en Conceptos</a>
+
+                                                        <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_08" className='Navigator__links'>Habilidades en Control de versiones</a>
+
+                                                        <a onClick={   ( ) => this.setState(   {  menu: toggleMenu  }   )   }  href="#sector_09" className='Navigator__links'>Contacto</a>
+
+                                                </div>
+
+                                                <div className='Navigator__listdot'>
+
+                                                        <div onClick={   ( )=> this.indexArrowClickMinus( )  }     className={  `Navigator__button__prev ${  Menu ? 'open': '' } `  }>
+                                                                <span></span>
+                                                                <span></span>
+                                                                <span></span>
+                                                        </div>
+
+                                                        <div className={  `Navigator__dot__container ${  Menu ? 'open': '' } `  } >
+                                                                
+                                                                <ul >
+
+                                                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
+                                                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
+                                                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
+                                                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
+                                                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
+                                                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
+                                                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
+                                                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
+                                                                        <li  onClick={   ( ) =>  this.IndexDotClick( )   }  className={  `Navigator__dot ${  Menu ? 'open': '' } `  } ></li> 
+
+                                                                </ul>
+                                                        </div>
+
+                                                        <div onClick={   ( )=> this.indexArrowClickPlus( )   }     className={  `Navigator__button__next ${  Menu ? 'open': '' } `  }  >
+                                                                <span></span>
+                                                                <span></span>
+                                                                <span></span>
+                                                        </div>
+                                                </div>
                                         </div>
-
-
-                                </div>
-
-
-
-
-
-                            </div>
-
-
-
-
-                        </nav>
+                                </nav>
                         ) ;
-                    }                  
-            }
+                }                  
+        }
 } ;
+
 export default NavegadorResp;
 
 
@@ -248,3 +226,4 @@ function BurguerButton()
 }
 export default BurguerButton
 */
+
